649f38b66353de21b8cc72f6e19151bc76858bd1
jeeljpaul <jeeljpaul2025@mca.ajce.in>
2025-03-19 04:05:22 +0530

    recommendation system modified

--- jewelrysystem/urls.py
+++ jewelrysystem/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('jewelryapp.urls')),
     path('admin/', admin.site.urls),
-]
+
+    path('recommendation/', include('recommendation.urls')),
+    
+
+
+        path("celebrity_match/", include("celebrity_match.urls")),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
